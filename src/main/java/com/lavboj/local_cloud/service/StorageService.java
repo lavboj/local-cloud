ab500ed548a1837b791f26475fb25a22d33e5fd1
lavboj <58738866+lavboj@users.noreply.github.com>
2025-07-31 22:39:32 +0200

    Implemented recursive deleteDir method

--- src/main/java/com/lavboj/local_cloud/service/StorageService.java
+++ src/main/java/com/lavboj/local_cloud/service/StorageService.java
@@ -1,17 +1,19 @@
 package com.lavboj.local_cloud.service;
+import com.lavboj.local_cloud.model.FileItem;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.stereotype.Service;
-
-import com.lavboj.local_cloud.model.FileItem;
 
 @Service
 public class StorageService {
@@ -70,6 +72,39 @@ public class StorageService {
         } else {
             throw new IllegalArgumentException("This name is already in use for file");
         }
+    }
 
+    public void deleteDirectory(String userPath, String directoryName, boolean confirmed) throws IOException {
+        Path targetPath = rootPath.resolve(userPath).resolve(directoryName).normalize();
+
+        if(!targetPath.startsWith(rootPath)) {
+            throw new IllegalArgumentException("Access to the specified path is not allowed.");
+        }
+
+        if (!Files.exists(targetPath) || !Files.isDirectory(targetPath)) {
+            throw new NoSuchFileException("Directory does not exist: " + targetPath);
+        }
+
+        if(checkContent(targetPath).equals(DirContent.EMPTY)) {
+            Files.delete(targetPath);
+        } else if(confirmed) {
+            Files.walkFileTree(targetPath, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException e) throws IOException{
+                    if (e == null) {
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    } else {
+                        throw e;
+                    }
+                }});
+        } else {
+            throw new IllegalStateException("Directory is not empty, confirmation required to delete.");
+        }
     }
 }
