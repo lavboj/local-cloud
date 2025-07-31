66f70532180a8ce05bac9ce36abdcd7daddf16ff
lavboj <58738866+lavboj@users.noreply.github.com>
2025-07-31 18:44:21 +0200

    Implemented /create rest controller

--- src/main/java/com/lavboj/local_cloud/controller/StorageController.java
+++ src/main/java/com/lavboj/local_cloud/controller/StorageController.java
@@ -3,6 +3,8 @@ package com.lavboj.local_cloud.controller;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lavboj.local_cloud.model.FileItem;
 import com.lavboj.local_cloud.service.StorageService;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 @RequestMapping("/api/storage")
@@ -25,5 +29,19 @@ public class StorageController {
     public List<FileItem> getContent(@RequestParam(defaultValue = "") String userPath) throws IOException {
         return storageService.getContent(userPath);
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createDirectory(
+            @RequestParam(defaultValue = "") String userPath,
+            @RequestParam(required = true) String directoryName
+    ){
+        try {
+            storageService.createDirectory(userPath, directoryName);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Creation was succesfull");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This name is already in use");
+        }
+    }
+    
 
 }
