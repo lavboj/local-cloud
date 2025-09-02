1969e43a3bbae174462a4a6e76c7b6cfb4635def
lavboj <58738866+lavboj@users.noreply.github.com>
2025-09-02 09:44:20 +0200

    Added parameters to file and its getters

--- src/main/java/com/lavboj/local_cloud/model/FileItem.java
+++ src/main/java/com/lavboj/local_cloud/model/FileItem.java
@@ -3,17 +3,29 @@ package com.lavboj.local_cloud.model;
 public class FileItem {
     private String name;
     private boolean isDirectory;
+    private long size;
+    private String modified;
 
-    public FileItem(String name, boolean isDirectory) {
+    public FileItem(String name, boolean isDirectory, long size, String modified) {
         this.name = name;
         this.isDirectory = isDirectory;
+        this.size = size;
+        this.modified = modified;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public boolean isDirectory() {
-        return isDirectory;
+        return this.isDirectory;
+    }
+
+    public long getSize() {
+        return this.size;
+    }
+
+    public String getModified() {
+        return this.modified;
     }
 }
